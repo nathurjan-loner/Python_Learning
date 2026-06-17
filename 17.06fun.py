17ccbf9ed4af5333bf39cb407bbe345266bbeb91
nathurjan-loner <nathurjan123@gmail.com>
2026-06-17 20:11:08 +0530

    17.06fun

---
+++ 17.06fun.py
@@ -0,0 +1,10 @@
+a = int(input("Enter a number: "))
+b = int(input("Enter the second number: "))
+
+if a > b:
+    print(f"{a} is greater than {b}")
+elif a < b:
+    print(f"{b} is greater than {a}")
+else:
+    print("Both numbers are equal")
+
